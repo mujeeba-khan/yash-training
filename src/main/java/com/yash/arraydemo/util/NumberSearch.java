7dc77a21708cfdd4f901dcedfe1409303043728d
unknown <mujeeba2230@gmail.com>
2021-07-17 13:37:07 +0530

    first commit

---
+++ src/main/java/com/yash/arraydemo/util/NumberSearch.java
@@ -0,0 +1,12 @@
+package com.yash.arraydemo.util;
+
+public class NumberSearch {
+
+	public int searchNumber(String string) {
+		
+		return null;
+	}
+
+	
+
+}
---
+++ src/test/java/com/yash/arraydemo/util/NumberSearchTest.java
@@ -0,0 +1,15 @@
+package com.yash.arraydemo.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class NumberSearchTest {
+
+	 @BeforeAll
+	    public static void runOnceBeforeClass() {
+	        NumberSearch num = new NumberSearch();
+	    }
+
+}
